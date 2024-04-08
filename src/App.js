3ea77f256c71522c9b318c1a68dbcb9cd2187d6d
Sarmad Raees Khan <raeessarmad200@gmail.com>
2024-04-09 00:20:49 +0200

    add routing and overall structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './App.css';
-
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Root, Home, NotFound } from "./pages";
+import UnderConstruction from "./components/UnderConstruction";
+import UserTable from "./components/UserTable";
+import AddUser from "./components/AddUser";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Container fluid>
+        <Routes>
+          <Route path='/*' element={<Main />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+const Main = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Home />} />
+          <Route path="/users" element={<UserTable />} />
+          <Route path="/add-user" element={<AddUser />} />
+          <Route path="/path2" element={<UnderConstruction />} />
+          <Route path="/path3" element={<UnderConstruction />} />
+          <Route path="/path4" element={<UnderConstruction />} />
+        </Route>
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
