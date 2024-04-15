523e1219e7608bc63308227ab20678a74b537b3e
Sarmad Raees Khan <raeessarmad200@gmail.com>
2024-04-15 15:42:05 +0200

    externalize firebase config using dotenv

--- src/services/config.js
+++ src/services/config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC-1Qba1nyjT04rQc-IpWheTeJ_jAiJNRw",
-  authDomain: "smart-home-portal-srk.firebaseapp.com",
-  projectId: "smart-home-portal-srk",
-  storageBucket: "smart-home-portal-srk.appspot.com",
-  messagingSenderId: "98327903811",
-  appId: "1:98327903811:web:750a2bdaa67958f5ed1ef6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export default firebaseConfig;
